a6f0089cfa2109568fc972c49959f309228573e4
sarthakpati <sarthak.pati@hotmail.com>
2025-07-18 09:51:33 -0400

    updated the minimum to a more modern version of python

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.9",
     install_requires=[
         "certifi",
         "cycler",
